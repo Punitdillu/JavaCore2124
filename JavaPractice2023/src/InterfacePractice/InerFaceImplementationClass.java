1d9b5cbf7a7c52c467dd396c5dbe2035f5a4eb24
Punitdillu <punitranjan2105@gmail.com>
2024-01-15 10:15:17 +0530

    Interface program

---
+++ JavaPractice2023/src/InterfacePractice/InerFaceImplementationClass.java
@@ -0,0 +1,26 @@
+package InterfacePractice;
+
+
+interface Test{
+	
+	String NAME= "Punit Ranjan";
+	
+	 void m1();
+		
+}
+
+public class InerFaceImplementationClass implements Test {
+
+	public static void main(String[] args) {
+
+		Test t = new InerFaceImplementationClass();
+		t.m1();
+		System.out.println(NAME);
+	}
+
+	@Override
+	public void m1() {
+		System.out.println("Hello interface");
+	}
+
+}
