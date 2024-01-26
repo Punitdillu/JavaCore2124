6a637c03256de228c19b4e89bdff533d724564cf
Punitdillu <punitranjan2105@gmail.com>
2024-01-26 08:19:40 +0530

    Collection codes

---
+++ JavaPractice2023/src/CollectionProg/ArrayListTest.java
@@ -0,0 +1,19 @@
+package CollectionProg;
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+
+	public static void main(String[] args) {
+		
+	 ArrayList l = new ArrayList();
+	 l.add(6);
+	 l.add("Hello");
+	 l.add('C');
+	 l.add(4.6);
+	 l.add("how r you");
+	 l.add(null);
+	 l.add(6);
+	 System.out.println(l);
+	}
+}
