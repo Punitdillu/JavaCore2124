e59ccec3418f7461de615834c9e73552b664a31d
Punitdillu <punitranjan2105@gmail.com>
2024-02-26 07:52:13 +0530

    commit 145

---
+++ JavaPractice2023/src/StringPrograming/Test.java
@@ -0,0 +1,17 @@
+package StringPrograming;
+
+public class Test {
+	
+	public static void main(String [] args)
+	{
+		int x = 2;
+		int y=5;
+		int z;
+		
+		z= x-y;
+		
+		System.out.print(z);
+		System.out.println();
+	}
+
+}
